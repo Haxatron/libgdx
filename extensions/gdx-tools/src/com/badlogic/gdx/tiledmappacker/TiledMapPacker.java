87f56d1ef86272a415c0cdb816c5eac698adb9aa
Haxatron <76475453+Haxatron@users.noreply.github.com>
2022-01-15 12:12:46 +0800

    Update TiledMapPacker.java

--- extensions/gdx-tools/src/com/badlogic/gdx/tiledmappacker/TiledMapPacker.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tiledmappacker/TiledMapPacker.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -359,6 +360,7 @@ public class TiledMapPacker {
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 
 		try {
+			docFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 			docBuilder = docFactory.newDocumentBuilder();
 			doc = docBuilder.parse(tmxFileHandle.read());
 
